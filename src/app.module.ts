0e762b24f0162eb4231215b21937a443db2db267
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2022-12-05 06:58:15 +0200

    0

--- src/app.module.ts
+++ src/app.module.ts
@@ -35,7 +35,7 @@ import { GatewayModule } from './gateway/gateway.module';
       // dropSchema: true,
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT as string),
+      port: 5432,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
